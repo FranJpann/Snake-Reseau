fbad4082d16333485692df61aae18aa742e7b141
Cady Loann <loann.cady@gmail.com>
2023-01-26 11:44:03 +0100

    Ajout fonctions deleteUser UpdatePseudo updatePassword

--- src/main/java/dao/UtilisateurDao.java
+++ src/main/java/dao/UtilisateurDao.java
@@ -9,4 +9,7 @@ public interface UtilisateurDao {
 	boolean isUserInDB(String pseudo, String password);
 	List<User> getUsers();
 	List<User> getLastUsers();
+	void deleteUser(User user);
+	public void updateUser(User user, String pseudo);
+	public void updatePassword(User user, String password);
 }
--- src/main/java/dao/UtilisateurDaoImpl.java
+++ src/main/java/dao/UtilisateurDaoImpl.java
@@ -28,7 +28,7 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
     	try {
 			connexion = daoFactory.getConnection();
 			statement = connexion.createStatement();
-			resultat = statement.executeQuery("SELECT id, pseudo FROM users WHERE pseudo='"+ pseudo + "' && password=MD5('"+ password +"');");
+			resultat = statement.executeQuery("SELECT id, pseudo FROM users WHERE pseudo='"+ pseudo + "' && password=SHA1('"+ password +"');");
 			
 			
 			if(resultat.next() == true) {
@@ -78,7 +78,6 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
     public List<User> getLastUsers(){
 		List<User> users = new ArrayList<User>();
 		
-		//Connexion à la base
 		Connection connexion = null;
 		Statement statement = null;
 		ResultSet resultat = null;
@@ -117,9 +116,62 @@ public class UtilisateurDaoImpl implements UtilisateurDao {
 		try {
 			connexion = daoFactory.getConnection();
 			
-			preparedStatement = connexion.prepareStatement("INSERT INTO users(pseudo, password) VALUES(?, MD5(?));");
+			preparedStatement = connexion.prepareStatement("INSERT INTO users(pseudo, password) VALUES(?, SHA1(?));");
 			preparedStatement.setString(1, user.getPseudo());
 			preparedStatement.setString(2, user.getPassword());
+			preparedStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteUser(User user) {
+		Connection connexion = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connexion = daoFactory.getConnection();
+			
+			preparedStatement = connexion.prepareStatement("DELETE FROM users WHERE pseudo=? AND password=SHA1(?);");
+			preparedStatement.setString(1, user.getPseudo());
+			preparedStatement.setString(2, user.getPassword());
+			preparedStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateUser(User user, String pseudo) {
+		Connection connexion = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connexion = daoFactory.getConnection();
+			
+			preparedStatement = connexion.prepareStatement("UPDATE users SET pseudo=? WHERE pseudo=? AND password=SHA1(?);");
+			preparedStatement.setString(2, user.getPseudo());
+			preparedStatement.setString(3,user.getPassword());
+			preparedStatement.setString(1,pseudo);
+			
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updatePassword(User user,String passwd) {
+		Connection connexion = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connexion = daoFactory.getConnection();
+			
+			preparedStatement = connexion.prepareStatement("UPDATE users SET password=SHA1(?) WHERE pseudo=? AND password=SHA1(?);");
+			preparedStatement.setString(2, user.getPseudo());
+			preparedStatement.setString(3,user.getPassword());
+			preparedStatement.setString(1,passwd);
 			
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
